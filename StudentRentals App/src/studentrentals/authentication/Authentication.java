88f095c08ea7448f7c0cf69994f39779063c6abb
Tyler <mitchelltb@cardiff.ac.uk>
2026-01-07 01:24:35 +0000

    Add Authentication

---
+++ StudentRentals App/src/studentrentals/authentication/Authentication.java
@@ -0,0 +1,52 @@
+package studentrentals.authentication;
+
+import studentrentals.repository.UserRepo;
+import studentrentals.util.IDManage;
+import studentrentals.util.Passwords;
+import studentrentals.domain.Student;
+import studentrentals.domain.User;
+import studentrentals.domain.Homeowner;
+
+import java.util.Optional;
+
+import studentrentals.util.Validate;
+
+
+public final class Authentication {
+    private final UserRepo userRepo;
+
+    public Authentication(UserRepo userRepo, String adminCreationPassword) {
+        this.userRepo = userRepo;
+    }
+
+    public Student registerStudent( String id, String name, String email, String password, String university, String student_ID){
+        Validate.notBlank(university, "University");
+        Validate.notBlank(student_ID, "Student ID");
+
+        Student student = new Student(id, name, email, Passwords.hashPassword(password), university, student_ID);
+        userRepo.saveUser(student);
+        return student;
+    }
+
+    public Homeowner registerHomeowner(String name, String email, String password){
+        Homeowner homeowner = new Homeowner(IDManage.generateUserID(), name, email, Passwords.hashPassword(password));
+        userRepo.saveUser(homeowner);
+        return homeowner;
+    }
+
+    public User loginAndAuth (String email, String password) {
+        Optional<User> user = userRepo.findUserByEmail(email);
+        if (user.isEmpty() || !user.get().isActive()) {
+            throw new IllegalArgumentException("Invalid email or account is deactivated");
+        }
+
+        String hash = Passwords.hashPassword(password);
+        String stored_hash = user.get().getPasswordHash();
+
+        if (!hash.equals(stored_hash)) {
+            throw new IllegalArgumentException("Invalid password");
+        }
+
+        return user.get();
+    }
+}
---
+++ StudentRentals App/src/studentrentals/util/IDManage.java
@@ -0,0 +1,28 @@
+package studentrentals.util;
+
+public class IDManage {
+
+    private static int user_count = 0;
+    private static int property_count = 0;
+    private static int booking_count = 0;
+    private static int room_count = 0;
+
+    private IDManage(){}
+
+    public static String generateUserID() {
+        user_count++;
+        return "U" + String.format("%05d", user_count);
+    }
+    public static String generatePropertyID() {
+        property_count++;
+        return "P" + String.format("%05d", property_count);
+    }
+    public static String generateBookingID() {
+        booking_count++;
+        return "B" + String.format("%05d", booking_count);
+    }
+    public static String generateRoomID() {
+        room_count++;
+        return "R" + String.format("%05d", room_count);
+    }
+}
